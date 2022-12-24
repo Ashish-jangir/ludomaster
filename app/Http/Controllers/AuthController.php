91fd8136e887a0813b8c2be515b541a51b6daeb4
Ashish Jangir <ashishjangirtechfan@gmail.com>
2022-12-24 19:40:37 +0530

    User Authentication System is implemented

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash; 
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+
+
+class AuthController extends Controller
+{
+    //
+    public function signup(Request $request) {
+        $fields = $request->validate([
+            'name'=>'required',
+            'email' => 'required | email | unique:users,email',
+            'phone' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            "name" => $fields['name'],
+            "email" => $fields['email'],
+            "phone" => $fields['phone'],
+            "password" => bcrypt($fields['password']),
+        ]);
+
+        if(is_null($user)) {
+            $response = [
+                'status' => false,
+                'msg' => 'there is a problem creating user'
+            ];
+            return response()->json($response, 500);
+        }
+
+        event(new Registered($user));
+        // $token = $user->createToken('Ludo7Master')->plainTextToken;
+
+        $response = [
+            'status' => true,
+            // 'token' => $token,
+            'user' => $user
+        ];
+        return response()->json($response, 201);
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'email' => 'required | email',
+            'password'=>'required',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(is_null($user)) {
+            $response = [
+                'status' => false,
+                'msg' => "Username or password is incorrect !"
+            ];
+            return response()->json($response, 400);
+        }
+        if(!Hash::check($fields['password'], $user->password)) {
+            $response = [
+                'status' => false,
+                'msg' => "Username or password is incorrect !"
+            ];
+            return response()->json($response, 400);
+        }
+
+        if(!$user->hasVerifiedEmail()) {
+            $response = [
+                'status' => false,
+                'msg' => "User Email is not verified. Please verify the email."
+            ];
+            return response()->json($response, 400);
+        }
+
+        $token = $user->createToken('Ludo7Master')->plainTextToken;
+
+        $response = [
+            'status' => true,
+            'token' => $token,
+            'user' => $user
+        ];
+        return response()->json($response, 500);
+    }
+
+    public function user(Request $request) {
+        return $request->user();
+    }
+
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
+        $response = [
+            "status" => true,
+            "msg" => "Logged Out",
+        ];
+        return response($response, 200);
+    }
+
+    public function forgot(Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $request['email'])->first();
+        if(is_null($user)) {
+            $response = [
+                'status' => false,
+                'msg' => "User not found with this email !"
+            ];
+            return response()->json($response, 400);
+        }
+        $user->tokens()->delete();
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status == Password::RESET_LINK_SENT) {
+            $response = [
+                "status" => true,
+                "msg" => "Password reset Link is sent to your email",
+            ];
+            return response($response, 200);
+        }
+
+    }
+    
+    public function reset(Request $request) {
+
+        $validated = $request->validateWithBag('updatePassword', [
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $request->user()->update([
+            'password' => Hash::make($validated['password']),
+        ]);
+        $user = $request-> user();
+        $user->tokens()->delete();
+        $token = $user->createToken('Ludo7Master')->plainTextToken;
+        $response = [
+            "status" => true,
+            "msg" => "Password Updated",
+            "token" => $token,
+        ];
+        return response($response, 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/', function(Request $request) {
+    return "Welcome";
+});
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+Route::post('/signup', [AuthController::class, 'signup']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/forgot-password', [AuthController::class, 'forgot']);
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/reset', [AuthController::class, 'reset']);
+});
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
